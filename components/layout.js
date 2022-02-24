975ec5cb51b1e25e928dace4a7cec6252186607b
tlmn <mail@tlmn.io>
2022-02-24 21:45:52 +0100

    set grid

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,10 @@
 import clsx from "clsx";
 
 const Layout = ({ className, children }) => (
-  <div className={clsx(className, "w-screen min-h-screen")}>
-    <div className="container py-16">{children}</div>
+  <div className={clsx(className, "w-screen min-h-screen py-16")}>
+    <div className="mx-16 sm:mx-32 md:mx-48 lg:mx-64 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-16 md:gap-32">
+      {children}
+    </div>
   </div>
 );
 
