f54bbdbf1a1d8d2d98d378f1750cfbd09ed3b6c6
tlmn <mail@tlmn.io>
2022-02-24 21:54:16 +0100

    remove unused dimension props

--- components/image.js
+++ components/image.js
@@ -1,13 +1,11 @@
 import { default as NextImage } from "next/image";
 import clsx from "clsx";
 
-const Image = ({ url, alt, height, width, className }) => (
+const Image = ({ url, alt, className }) => (
   <div className={clsx(className, "aspect-square relative")}>
     <NextImage
       src={url}
       alt={alt}
-      width={width}
-      height={height}
       layout="fill"
       objectFit="cover"
     />
