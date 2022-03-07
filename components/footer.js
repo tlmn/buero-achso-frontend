2872f3dd746423aeb20dcf592d8267556749dfdd
tlmn <mail@tlmn.io>
2022-03-07 19:03:54 +0100

    prettier

--- components/footer.js
+++ components/footer.js
@@ -1,22 +1,24 @@
 import useAppContext from "@/lib/useAppContext";
-import React from "react"
+import React from "react";
 import { useSpring, animated } from "react-spring";
+
 const Footer = ({ footer, footercopyright }) => {
-  const { appState } = useAppContext()
-  const { isBlurred } = appState
-  const animation = useSpring({ filter: isBlurred ? `blur(3px)` : `blur(0px)`, config: { duration: 100 } })
+  const { appState } = useAppContext();
+  const { isBlurred } = appState;
+  const animation = useSpring({
+    filter: isBlurred ? `blur(3px)` : `blur(0px)`,
+    config: { duration: 100 },
+  });
 
   return (
-    <animated.div
-      style={animation}
-      className="col-span-full">
+    <animated.div style={animation} className="col-span-full">
       <span dangerouslySetInnerHTML={{ __html: footer }} className="block" />
       <span
         dangerouslySetInnerHTML={{ __html: footercopyright }}
         className="block"
       />
     </animated.div>
-  )
+  );
 };
 
 export default Footer;
