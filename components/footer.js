b12f4fb14088e2a45f59aac49bedec6ec05faae9
tlmn <mail@tlmn.io>
2022-03-07 19:25:39 +0100

    add questionMark

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,8 @@
 import useAppContext from "@/lib/useAppContext";
+import Link from "next/link";
 import React from "react";
 import { useSpring, animated } from "react-spring";
+import QuestionMark from "./svg/questionMark";
 
 const Footer = ({ footer, footercopyright }) => {
   const { appState } = useAppContext();
@@ -12,6 +14,11 @@ const Footer = ({ footer, footercopyright }) => {
 
   return (
     <animated.div style={animation} className="col-span-full">
+      <Link href="ueber">
+        <div className="cursor-pointer my-1">
+          <QuestionMark className="w-[1.5rem]" />
+        </div>
+      </Link>
       <span dangerouslySetInnerHTML={{ __html: footer }} className="block" />
       <span
         dangerouslySetInnerHTML={{ __html: footercopyright }}
