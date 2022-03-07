0314ee82ce7416a168b061efef4df0999033a622
tlmn <mail@tlmn.io>
2022-03-07 18:40:19 +0100

    add navigation icons

---
+++ components/svg/closingX.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ClosingX = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="41"
+    height="42"
+    fill="none"
+    viewBox="0 0 41 42"
+  >
+    <path
+      fill="#000"
+      d="M.41.818l15.7 20.62L.35 42H8.67l11.599-15.114L31.867 42h8.319L24.369 21.438 40.186.818h-8.143L20.27 16.108 8.553.818H.41z"
+    ></path>
+  </svg>
+);
+
+export default ClosingX;
