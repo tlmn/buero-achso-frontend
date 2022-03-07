3bc96e0780ef6c11a81ad8bdcc78011f85c4a443
tlmn <mail@tlmn.io>
2022-03-07 18:56:34 +0100

    blur footer too

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,22 @@
-const Footer = ({ footer, footercopyright }) => (
-  <div className="col-span-full">
-    <span dangerouslySetInnerHTML={{ __html: footer }} className="block" />
-    <span
-      dangerouslySetInnerHTML={{ __html: footercopyright }}
-      className="block"
-    />
-  </div>
-);
+import useAppContext from "@/lib/useAppContext";
+import React from "react"
+import { useSpring, animated } from "react-spring";
+const Footer = ({ footer, footercopyright }) => {
+  const { appState } = useAppContext()
+  const { isBlurred } = appState
+  const animation = useSpring({ filter: isBlurred ? `blur(4px)` : `blur(0px)`, config: { duration: 100 } })
+
+  return (
+    <animated.div
+      style={animation}
+      className="col-span-full">
+      <span dangerouslySetInnerHTML={{ __html: footer }} className="block" />
+      <span
+        dangerouslySetInnerHTML={{ __html: footercopyright }}
+        className="block"
+      />
+    </animated.div>
+  )
+};
 
 export default Footer;
