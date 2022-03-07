0e84321932472bd1432aec569b9e6784c71687e8
tlmn <mail@tlmn.io>
2022-03-07 18:41:49 +0100

    pass down index as key

--- components/worksGallery/worksGallery.js
+++ components/worksGallery/worksGallery.js
@@ -3,7 +3,7 @@ import WorksGalleryItem from "@/components/worksGallery/worksGalleryItem";
 const WorksGallery = ({ works }) => (
   <>
     {works.map(
-      (item) => item.url !== "undefined" && <WorksGalleryItem workitem={item} />
+      (item, index) => item.url !== "undefined" && <WorksGalleryItem workitem={item} index={index} />
     )}
   </>
 );
