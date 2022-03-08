fd1a4a2df5707f99a02d7de2d2fbfc824836e88f
tlmn <mail@tlmn.io>
2022-03-08 10:41:25 +0100

    fix blurred state

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import "@/styles/global.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Provider as AppContextProvider } from "@/lib/useAppContext";
+import { useRouter } from "next/router";
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
   const [appState, setAppState] = useState({
     claimID: 0,
     isBlurred: false,
     currentItem: 0,
   });
+
+  const handleRoute = () =>
+    setAppState((prev) => ({ ...prev, isBlurred: false }));
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRoute);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRoute);
+    };
+  }, [router]);
 
   return (
     <AppContextProvider value={{ appState, setAppState }}>
