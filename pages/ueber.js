36538e6d8ec816248ed7e4f88b5a09fa99985dcb
tlmn <mail@tlmn.io>
2022-03-11 16:04:02 +0100

    fix text issues

--- pages/ueber.js
+++ pages/ueber.js
@@ -8,7 +8,7 @@ import { useState } from "react";
 const AboutPage = ({
   sitemeta,
   pagemeta,
-  pagecontent: { content, portraitimage, portraitimagehover, socialmedia },
+  pagecontent: { contactinfo, text, content, portraitimage, portraitimagehover, socialmedia },
 }) => {
   const [isMouseOver, setIsMouseOver] = useState(false);
   return (
@@ -35,7 +35,7 @@ const AboutPage = ({
           </div>
         </div>
         <div className="col-span-1">
-          <span dangerouslySetInnerHTML={{ __html: content.contactinfo }} />
+          <span dangerouslySetInnerHTML={{ __html: contactinfo }} />
           <div>
             {socialmedia.map(({ socialmedianame, socialmediaurl }) => (
               <a
@@ -46,7 +46,7 @@ const AboutPage = ({
           </div>
         </div>
         <div className="col-span-1 col-start-1">
-          <span dangerouslySetInnerHTML={{ __html: content.text }} />
+          <span dangerouslySetInnerHTML={{ __html: text }} />
         </div>
         <Footer {...sitemeta} />
       </Layout>
@@ -61,6 +61,12 @@ export async function getStaticProps() {
         query: "page('ueber')",
         select: {
           content: true,
+          contactinfo: {
+            query: "page('ueber').content.contactinfo.markdown"
+          },
+          text: {
+            query: "page('ueber').content.text.markdown"
+          },
           portraitimage: "page('ueber').content.portraitimage.toFile",
           portraitimagehover: "page('ueber').content.portraitimagehover.toFile",
           socialmedia: "page('ueber').content.socialmedia.toStructure",
@@ -69,7 +75,7 @@ export async function getStaticProps() {
       ...metaQuery("ueber"),
     },
   });
-
+  console.log(result);
   return { props: result };
 }
 
