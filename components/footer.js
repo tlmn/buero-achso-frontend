5428171d6f389b088a1969dc33f5db7be96299ea
tlmn <mail@tlmn.io>
2022-03-14 20:57:21 +0100

    fix styling

--- components/footer.js
+++ components/footer.js
@@ -8,10 +8,13 @@ const Footer = ({ footer, footercopyright }) => (
   <div className="col-span-full">
     <Link href="/ueber">
       <div className="cursor-pointer my-1 hover:blurred inline-block">
-        <QuestionMark className="w-[1.5rem]" />
+        <QuestionMark className="w-[20px] sm:w-[40px]" />
       </div>
     </Link>
-    <span dangerouslySetInnerHTML={{ __html: footer }} className="block hover:blurred" />
+    <span
+      dangerouslySetInnerHTML={{ __html: footer }}
+      className="block hover:blurred"
+    />
     <span
       dangerouslySetInnerHTML={{ __html: footercopyright }}
       className="block hover:blurred"
