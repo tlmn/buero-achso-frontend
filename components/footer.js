fc3ef543e52cd616ff0b43cb583368c6b4fa78a8
tlmn <mail@tlmn.io>
2022-05-12 12:43:19 +0200

    reduce size and spacing

--- components/footer.js
+++ components/footer.js
@@ -5,8 +5,8 @@ import QuestionMark from "./svg/questionMark";
 const Footer = ({ footer, footercopyright }) => (
   <div className="col-span-full">
     <Link href="/ueber">
-      <div className="cursor-pointer my-1 hover:blurred inline-block">
-        <QuestionMark className="w-[20px] sm:w-[40px]" />
+      <div className="cursor-pointer hover:blurred inline-block">
+        <QuestionMark className="w-[20px] h-[20px] aspect-ratio" />
       </div>
     </Link>
     <span
