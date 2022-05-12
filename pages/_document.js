10f6e35f5ef4de1a167ab57d7c1024e12f709467
tlmn <mail@tlmn.io>
2022-05-12 12:31:28 +0200

    fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html lang="de">
     <Head>
-      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <body>
       <Main />
